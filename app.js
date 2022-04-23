b771f312c7914c95bb2bb04acf87ce830483c059
carocollazo <67442372+carocollazo@users.noreply.github.com>
2022-04-22 23:21:36 -0500

    final

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const app = express();
 
-app.listen(3000, () => {
-  console.log("Apppcation started and pstening on port 3000");
-});
+// app.listen(3000, () => {
+//   console.log("Apppcation started and pstening on port 3000");
+// });
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
