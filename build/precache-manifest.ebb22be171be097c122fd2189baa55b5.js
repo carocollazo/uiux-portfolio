4d6e7a76f1ea031554b5d55f7ba75ace75d91105
carocollazo <67442372+carocollazo@users.noreply.github.com>
2022-04-22 23:22:17 -0500

    final

---
+++ build/precache-manifest.ebb22be171be097c122fd2189baa55b5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4aeabc32a3d92b8fae8",
+    "url": "/uiux-portfolio/static/css/main.9c890dba.chunk.css"
+  },
+  {
+    "revision": "f4aeabc32a3d92b8fae8",
+    "url": "/uiux-portfolio/static/js/main.f4aeabc3.chunk.js"
+  },
+  {
+    "revision": "91d1d4c5a6814aad6413",
+    "url": "/uiux-portfolio/static/js/1.91d1d4c5.chunk.js"
+  },
+  {
+    "revision": "3a48f6dde450dce09e8d",
+    "url": "/uiux-portfolio/static/js/2.3a48f6dd.chunk.js"
+  },
+  {
+    "revision": "8ad9732fc4c7c381ca2f",
+    "url": "/uiux-portfolio/static/js/runtime~main.8ad9732f.js"
+  },
+  {
+    "revision": "27ed4c0b74066c8bb0ab8b5bfb2afe88",
+    "url": "/uiux-portfolio/static/media/xkcd.27ed4c0b.png"
+  },
+  {
+    "revision": "8f05f02f4ee0de753165317c657c2e7f",
+    "url": "/uiux-portfolio/static/media/ss1.8f05f02f.png"
+  },
+  {
+    "revision": "67cd1a16b44425ac6b6f040615d79d11",
+    "url": "/uiux-portfolio/static/media/ss2.67cd1a16.png"
+  },
+  {
+    "revision": "ba8663c6bb3ebffb4d2dac3d13bf55e4",
+    "url": "/uiux-portfolio/static/media/ss3.ba8663c6.png"
+  },
+  {
+    "revision": "83ff18052abd98822d5928b00971ffb9",
+    "url": "/uiux-portfolio/static/media/ss4.83ff1805.jpg"
+  },
+  {
+    "revision": "98004473d3204fb133adca428a65e3d0",
+    "url": "/uiux-portfolio/index.html"
+  }
+];
